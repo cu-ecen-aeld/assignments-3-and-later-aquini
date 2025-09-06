65580bf1ae0aa105dd271f01738bc349ecd35e52
Rafael Aquini <raaquini@gmail.com>
2025-09-06 08:58:53 -0400

    assignment-4-part-2: Make required modifications for the finder-app
    Signed-off-by: Rafael Aquini <raaquini@gmail.com>

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,14 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+SCRIPTPATH="$(dirname $0)"
+CONFBASE="./"
+
+if [ -d "/etc/finder-app/conf" ]; then
+	CONFBASE="/etc/finder-app/"
+fi
+
+username=$(cat ${CONFBASE}conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +39,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
+assignment=`cat ${CONFBASE}conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -53,12 +60,14 @@ fi
 #make clean
 #make
 
-for i in $( seq 1 $NUMFILES)
+for i in $(seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	${SCRIPTPATH}/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(${SCRIPTPATH}/finder.sh "$WRITEDIR" "$WRITESTR")
+
+echo "${OUTPUTSTRING}" > /tmp/${assignment}-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
