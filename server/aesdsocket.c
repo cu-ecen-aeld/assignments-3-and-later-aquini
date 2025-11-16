a9e536b96b1425b4ed56edd04d9cd0382fc03e69
Rafael Aquini <raaquini@gmail.com>
2025-11-16 12:04:20 -0500

    assignment-8: socket server: implement requested modifications for aesdchar driver usage
    Signed-off-by: Rafael Aquini <raaquini@gmail.com>

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -27,7 +27,11 @@ static bool signal_exit = false;
 static bool do_work = false;
 static int port = 9000;
 const char *prog_name;
+#ifdef USE_AESD_CHAR_DEVICE
+const char *file = "/dev/aesdchar";
+#else
 const char *file = "/var/tmp/aesdsocketdata";
+#endif
 const char *short_opts = "hdp:f:";
 const struct option long_opts[] = {
 	{"help",	0, NULL, 'h'},
@@ -45,11 +49,11 @@ struct thread_desc {
 	SLIST_ENTRY(thread_desc) list;
 };
 
-int handle_request(int socket_fd, FILE *stream);
+int handle_request(int socket_fd, char *file);
 char *read_line(int fd);
 void *monitor_worker(void *arg);
 void *request_worker(void *arg);
-void write_timestamp(FILE *stream);
+void write_timestamp(char *file);
 
 void print_usage(void)
 {
@@ -150,6 +154,7 @@ int main(int argc, char *argv[])
 	stream = fopen(file, "a+");
 	if (!stream)
 		panic("fopen()", errno);
+	fclose(stream);
 
 	/* set up signal handler actions for catching common termination signals */
 	memset(&sa, 0, sizeof (sa));
@@ -173,7 +178,7 @@ int main(int argc, char *argv[])
 	if (!monitor)
 		panic("calloc()", errno);
 
-	monitor->data[0] = stream;
+	monitor->data[0] = (void *)file;
 	monitor->done = false;
 	rc = pthread_create(&monitor->id, NULL, monitor_worker, monitor);
 	if (rc != 0)
@@ -232,7 +237,7 @@ int main(int argc, char *argv[])
 			continue;
 		}
 
-		client->data[0] = stream;
+		client->data[0] = (void *)file;
 		client->data[1] = (void *)(unsigned long)request_fd;
 		client->done = false;
 		SLIST_INSERT_HEAD(&threads, client, list);
@@ -265,30 +270,37 @@ signal_out:
 
 	shutdown(socket_fd, SHUT_RDWR);
 	close(socket_fd);
-	fclose(stream);
+#ifndef USE_AESD_CHAR_DEVICE
 	unlink(file);
+#endif
 	closelog();
 
 	return 0;
 }
 
-void write_timestamp(FILE *stream)
+void write_timestamp(char *file)
 {
 	char str[512];
 	struct tm *tm_info;
 	time_t t;
+	FILE *stream;
+
+	stream = fopen(file, "a");
+	if (!stream)
+		return;
 
 	time(&t);
 	tm_info = localtime(&t);
 	strftime(str, sizeof (str), "timestamp: %a, %d %b %Y %T %z", tm_info);
 	fprintf(stream, "%s\n", str);
 	fflush(stream);
+	fclose(stream);
 }
 
 void *monitor_worker(void *arg)
 {
 	struct thread_desc *desc = arg;
-	FILE *stream = desc->data[0];
+	char *logfile = desc->data[0];
 
 	for (;;) {
 		if (signal_exit)
@@ -301,9 +313,11 @@ void *monitor_worker(void *arg)
 
 		/* janitorial work here */
 		if (!(sequence % 10)) {
+#ifndef USE_AESD_CHAR_DEVICE
 			pthread_mutex_lock(&log_write_mutex);
-			write_timestamp(stream);
+			write_timestamp(logfile);
 			pthread_mutex_unlock(&log_write_mutex);
+#endif
 		}
 	}
 
@@ -315,10 +329,10 @@ void *monitor_worker(void *arg)
 void *request_worker(void *arg)
 {
 	struct thread_desc *desc = arg;
-	FILE *stream = desc->data[0];
+	char *logfile = desc->data[0];
 	int rc, socket_fd = (unsigned long)desc->data[1];
 
-	rc = handle_request(socket_fd, stream);
+	rc = handle_request(socket_fd, logfile);
 	if (rc < 0)
 		warn("handle_request", errno);
 
@@ -330,13 +344,14 @@ void *request_worker(void *arg)
 	return NULL;
 }
 
-int handle_request(int socket_fd, FILE *stream)
+int handle_request(int socket_fd, char *file)
 {
 	struct sockaddr_in peer_addr;
 	socklen_t socket_len = 0;
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t nread;
+	FILE *stream;
 
 	memset(&peer_addr, 0, sizeof (peer_addr));
 	if (getpeername(socket_fd, (struct sockaddr *)&peer_addr, &socket_len) < 0)
@@ -350,13 +365,21 @@ int handle_request(int socket_fd, FILE *stream)
 		return EXIT_FAILURE;
 
 	pthread_mutex_lock(&log_write_mutex);
+	stream = fopen(file, "a");
+	if (!stream)
+		goto skip_log;
+
 	fprintf(stream, "%s\n", line);
 	fflush(stream);
+	fclose(stream);
 	free(line);
+skip_log:
+	stream = fopen(file, "r");
+	if (!stream)
+		goto skip_echo;
 
 	/* read file and send its whole content back through the socket */
 	line = NULL;
-	fseek(stream, 0, SEEK_SET);
 	while ((nread = getline(&line, &len, stream)) != -1) {
 		ssize_t nwrite = 0;
 
@@ -368,9 +391,12 @@ int handle_request(int socket_fd, FILE *stream)
 				panic("write()", errno);
 		}
 	}
+
+	fclose(stream);
+	free(line);
+skip_echo:
 	pthread_mutex_unlock(&log_write_mutex);
 
-	free(line);
 	syslog(LOG_INFO, "Closed connection from %s",
 			inet_ntoa(peer_addr.sin_addr));
 
